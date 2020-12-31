31ac0facd2acc8c0b72afc45ddbafd8244f8a3ce
pratham022 <pthombre657@gmail.com>
2020-12-31 11:55:53 +0530

    Refractor Nav to its own component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -2,21 +2,17 @@ import React, { Component } from "react";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
 import { ChromePicker } from 'react-color';
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import { arrayMove } from 'react-sortable-hoc';
 
-import DraggableColorBox from './DraggableColorBox';
 import DraggableColorList from './DraggableColorList';
+import PaletteFormNav from './PaletteFormNav';
 
 const drawerWidth = 400;
 
@@ -87,7 +83,6 @@ class NewPaletteForm extends Component {
     background: '#3434B8',
     newColorName: '',
     colors: this.props.palettes[0].colors,
-    newPaletteName: ''
   };
 
   componentDidMount() {
@@ -103,11 +98,6 @@ class NewPaletteForm extends Component {
         ))
     ));
 
-    ValidatorForm.addValidationRule('isPaletteNameUnique', value => (
-      this.props.palettes.every(currPalette => (
-          currPalette.paletteName.toLowerCase() !== value.toLowerCase()
-      ))
-  ));
   }
 
   handleChangeComplete = (color) => {
@@ -143,8 +133,8 @@ class NewPaletteForm extends Component {
     // })
   }
 
-  handleSubmit = () => {
-    let newColorName = this.state.newPaletteName;
+  handleSubmit = (newPaletteName) => {
+    let newColorName = newPaletteName;
     const newPalette = {
       paletteName: newColorName,
       id: newColorName.toLowerCase().replace(/ /g, '-'),
@@ -181,48 +171,19 @@ class NewPaletteForm extends Component {
   }
 
   render() {
-    const { classes, maxColors } = this.props;
+    const { classes, maxColors, palettes } = this.props;
     const { open, colors } = this.state;
 
     const paletteIsFull = colors.length >= maxColors;
 
     return (
       <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position='fixed'
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open
-          })}
-          color='default'
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color='inherit'
-              aria-label='Open drawer'
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant='h6' color='inherit' noWrap>
-              Persistent drawer
-            </Typography>
-            <ValidatorForm onSubmit={this.handleSubmit}>
-              <TextValidator 
-                  label='Palette Name' 
-                  value={this.state.newPaletteName} 
-                  name='newPaletteName' 
-                  onChange={this.handleChange} 
-                  validators={['required', 'isPaletteNameUnique']} 
-                  errorMessages={['Enter Palette Name', 'Palette Name already used']}/>
-              <Button
-                variant='contained' 
-                color='primary' 
-                type='submit'>Save Palette</Button>
-            </ValidatorForm>
-          </Toolbar>
-        </AppBar>
+        <PaletteFormNav 
+          open={open} 
+          classes={classes} 
+          palettes={palettes} 
+          handleSubmit={this.handleSubmit} 
+          handleDrawerOpen={this.handleDrawerOpen}/>
         <Drawer
           className={classes.drawer}
           variant='persistent'
