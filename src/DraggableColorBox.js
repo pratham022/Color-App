ae903539e5502bd13c64db2676e821d9f362fad5
pratham022 <pthombre657@gmail.com>
2021-01-02 12:19:32 +0530

    Move Styles out and add constants file

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -4,36 +4,8 @@ import { withStyles } from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import {SortableContainer, SortableElement} from 'react-sortable-hoc';
 
-const styles = {
-    root: {
-        width: '20%',
-        height: '25%',
-        margin: '0 auto',
-        display: 'inline-block',
-        position: 'relative',
-        marginBottom: '-3.5px',
-        '&:hover svg': {
-            color: 'white',
-            transform: 'scale(1.5)'
-        }
-    },
-    boxContent: {
-        position: 'absolute',
-        padding: '10px',
-        width: '100%',
-        left: '0px',
-        bottom: '0px',
-        letterSpacing: '1px',
-        textTransform: 'uppercase',
-        fontSize: '12px',
-        display: 'flex',
-        justifyContent: 'space-between'
-    },
-    deleteIcon: {
-        color: 'rgba(0, 0, 0, 0.5)',
-        transition: 'all 0.3s ease-in-out'
-    }
-}
+import styles from './styles/DraggableColorBoxStyles';
+
 
 const DraggableColorBox = SortableElement((props) => {
     const {classes, handleClick, name, color} = props;
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -12,50 +12,9 @@ import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import { Link } from 'react-router-dom';
 
 import PaletteMetaForm from './PaletteMetaForm';
+import styles from './styles/PaletteFormNavStyles';
 
 
-const drawerWidth = 400;
-
-const styles = theme => ({
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '64px'
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20
-  },
-  hide: {
-    display: "none"
-  },
-  navBtns: {
-    marginRight: '1rem'
-  },
-  button: {
-    margin: '0 0.5rem',
-  },
-  link: {
-    textDecoration: 'none'
-  }
-
-});
 
 class PaletteFormNav extends Component {
     constructor(props) {
